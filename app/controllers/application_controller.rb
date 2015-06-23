ec68fa9872302c8e2e091f8d0b4cb80930b4add2
Darya Murauyova <muravkina@yahoo.com>
2015-06-23 15:34:48 -0400

    created a student show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :is_hundred_days?
+
+  def is_hundred_days?(cohort_id)
+    start_date = Cohort.find(cohort_id).start_date
+    date_diff = Date.today - start_date
+    date_diff >= 100
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,8 @@
 class StudentsController < ApplicationController
 
+  # before_action :authorize, except: [:create, :new]
+
+  def show
+      @student = Student.find(params[:id])#session[:student_id])
+  end
 end
