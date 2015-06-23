2dd0118e78b5c8747bb3b0a4936bbc8a0658b992
Domenica LaLima <dlalima@yahoo.com>
2015-06-23 15:24:42 -0400

    fixed classes to cohorts

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,3 +1,13 @@
 class CohortsController < ApplicationController
 
+	def index
+	@cohorts = Cohort.all
+	render :index
+	end
+
+	def show
+	@cohort = Cohort.find(params[:id])
+	render :show
+	end
+
 end
