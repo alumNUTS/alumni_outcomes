ac3539e6bbc30f32a1b349a0a8123f0f502128e9
Mark Paulson <markpaulson1@hotmail.com>
2015-06-23 14:37:39 -0400

    update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,28 @@
 class StudentsController < ApplicationController
 
+def new
+@student = Student.new
+end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+def update
+@student = Student.find(params[:id])
+	if @student.update(student_params)
+			redirect_to "/students/#{@student.id}"
+		else
+			render :edit
+		end
+end
+
+
+end
+
+
+private
+
+def student_params
+	params.require(:student).permit(:name, :email, :phone_number, :city, :state, :password_digest, :skills, :is_employed, :employment_date, :company_name, :status, :survey_complete)
 end
