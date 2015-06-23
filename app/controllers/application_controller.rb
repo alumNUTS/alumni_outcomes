1ccc4dfba432da2b53f25b0494e13d26c503426e
Darya Murauyova <muravkina@yahoo.com>
2015-06-23 16:57:39 -0400

    updated student show page, added status methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :is_hundred_days?
-
+  before_action :alumni, :outcomes
   def is_hundred_days?(cohort_id)
     start_date = Cohort.find(cohort_id).start_date
     date_diff = Date.today - start_date
     date_diff >= 100
   end
+
+  def outcomes
+    students = Student.all
+    students.each do |student|
+      if (Date.today - student.cohort.end_date) > 0
+        student.status = "outcome"
+      end
+    end
+  end
+
+  def alumni
+    students = Student.all
+    students.each do |student|
+      if student.is_employed && student.survey_complete
+        student.status = "alumnus"
+      end
+  end
+
+  def
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
-  # before_action :authorize, except: [:create, :new]
+  before_action :authorize, except: [:create, :new, :show]
+
 
   def show
       @student = Student.find(params[:id])#session[:student_id])
   end
+
+
+
+
 end
