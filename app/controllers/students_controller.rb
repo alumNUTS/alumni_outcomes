6b2a7167cc4eb92f251a6ebdb249b8ecba10510e
Orin Xie <orin.xie@gmail.com>
2015-06-24 12:24:03 -0400

    fixed bugs with creating and editing a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,19 +12,31 @@ class StudentsController < ApplicationController
 
   def new
     @student = Student.new
+    @cohorts = Cohort.all
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to "/students/"
+    else
+      render :new
+    end
   end
 
 	def edit
 		@student = Student.find(params[:id])
+    @cohorts = Cohort.all
 	end
 
   def update
     @student = Student.find(params[:id])
-	 if @student.update(student_params)
-		redirect_to "/students/#{@student.id}"
-	 else
-		render :edit
-	 end
+
+	  if @student.update(student_params)
+		  redirect_to "/students/#{@student.id}"
+	  else
+		  render :edit
+	  end
   end
 
   def destroy
@@ -41,7 +53,7 @@ def students_page?
 end
 
 def student_params
-  params.require(:student).permit(:name, :email, :phone_number, :city, :state, :password_digest, :skills, :is_employed, :employment_date, :company_name, :status, :survey_complete)
+  params.require(:student).permit(:name, :email, :cohort_id, :phone_number, :city, :state, :password_digest, :skills, :is_employed, :employment_date, :company_name, :status, :survey_complete)
 end
 
 def logged_in?
