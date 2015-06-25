adc5b0b9fb2fed83fa57189982dae9d633deb753
Darya Murauyova <muravkina@yahoo.com>
2015-06-25 19:23:56 -0400

    only logged in student can complete the survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,30 +1,36 @@
 class SurveysController < ApplicationController
 
 	def index
-		@student = Student.find(params[:student_id])
-		@survey = Survey.new
+    if logged_in?
+		  @student = Student.find(params[:student_id])
+		  @survey = Survey.new
+    else
+      redirect_to '/login', notice: "Please, log in to complete the survey"
+    end
 
 	end
 
 	def create
-    if current_user.id != params[:student_id]
-  		@survey = Survey.new
-      
-      if @survey.save
-      	@student = Student.find(params[:student_id])
-      	@student.survey_complete = true
-      	@student.save
-        flash.keep[:notice] = "You've successfully submitted your survey!"
-      	redirect_to @student
-    	else 
+
+      if current_user.id != params[:student_id]
+    		@survey = Survey.new
+
+        if @survey.save
+        	@student = Student.find(params[:student_id])
+        	@student.survey_complete = true
+        	@student.save
+          flash.keep[:notice] = "You've successfully submitted your survey!"
+        	redirect_to @student
+      	else
+          @student = Student.find(params[:student_id])
+      		render :index
+      	end
+      else
         @student = Student.find(params[:student_id])
-    		render :index
-    	end
-    else
-      @student = Student.find(params[:student_id])
-      @error = "Are you NUTS? This isn't your survey"
-      render :index
-    end
+        @error = "Are you NUTS? This isn't your survey"
+        render :index
+      end
+
    end
 
 end
