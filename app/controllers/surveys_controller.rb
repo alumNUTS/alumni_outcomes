9400e627ca0653ea607e8fb8ee094557afe0101e
Domenica LaLima <dlalima@yahoo.com>
2015-06-25 18:03:26 -0400

    saving survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -3,21 +3,29 @@ class SurveysController < ApplicationController
 	def index
 		@student = Student.find(params[:student_id])
 		@survey = Survey.new
+
 	end
 
 	def create
-		@survey = Survey.new
-    
-    if @survey.save
-    	@student = Student.find(params[:student_id])
-    	@student.survey_complete = true
-    	@student.save
-
-    	redirect_to "/students/", notice: "You've successfully submitted your survey!"
-  	else 
-  		render :index
-  	end
-  end
+    # if current_user.id != params[:student_id]
+  		@survey = Survey.new
+      
+      if @survey.save
+      	@student = Student.find(params[:student_id])
+      	@student.survey_complete = true
+      	@student.save
+        flash.keep[:notice] = "You've successfully submitted your survey!"
+      	redirect_to @student
+    	else 
+        @student = Student.find(params[:student_id])
+    		render :index
+    	end
+  #   else
+  #     @student = Student.find(params[:student_id])
+  #     @error = "Are you NUTS? This isn't your survey"
+  #     render :index
+  #   end
+   end
 
 end
 
