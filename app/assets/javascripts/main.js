3bfed255cfb41576e33e24a45c9096d56db151c4
Orin Xie <orin.xie@gmail.com>
2015-06-26 13:57:22 -0400

    finished silhoutte animation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 	$(".notice_survey").on("click", function(){
 		$(".notice").hide()
 	})
+
+	//silhoutte animation
+	var count = 1
+  var starting = setInterval(function () {
+    $('#person' + count).fadeIn(100)
+    count += 1
+    if (count > 30) {
+    clearInterval(starting)
+    }
+  }, 100)
+
 })
