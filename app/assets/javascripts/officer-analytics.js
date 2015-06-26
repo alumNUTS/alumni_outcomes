2aaefa2f4d49b80d07cc447ab6da62f5f967ae98
Bobby King <king.robertjoseph@gmail.com>
2015-06-26 15:59:37 -0400

    Adds comments to js files

---
+++ app/assets/javascripts/officer-analytics.js
@@ -0,0 +1,51 @@
+	$(document).ready(function(){
+	
+	// Pulling cohort stats from the data attribute from html element id="officer_stats"
+	var cohortStats = $('.officer_stats').data('stats');	
+
+	// Creatign an empty array to store cohort employment percentages
+	myCohortsEmployment = [];
+
+	// Looping through cohort stats to push the employment rates 
+	// of ONLY the cohorts that graduated >= 90 days ago
+	for(var i = 0; i < cohortStats.length; i++){
+		if(cohortStats[i]['days_til_survey'] <= 10 ){
+			myCohortsEmployment.push(cohortStats[i]['employed'])
+		}
+	}
+
+	// Creates Pie Chart on officer's show page iff there is more than 1 cohort
+	// that graduated >= 90 days ago
+	if(myCohortsEmployment.length > 0){
+
+		//Getting sum of all employment rates for use in finding the average
+		var sum = 0;
+		for(var i = 0; i < myCohortsEmployment.length; i++){
+			sum += myCohortsEmployment[i];
+		}
+
+		// Getting average employment from 
+		avgEmployment = Math.floor(sum / myCohortsEmployment.length);
+
+		//Establishing charts
+		var $ctxP = $('#myPieChart').get(0).getContext('2d');
+		var myNewChartP = new Chart($ctxP);
+
+		// Employment pie chart data
+		var pieData = [
+		{
+			value: avgEmployment,
+			label: 'Employed',
+			color: '#f0553a'
+		},
+		{
+			value: 100 - avgEmployment,
+			label: 'Unemployed',
+			color: '#fedd91'
+		}
+		]
+
+		// Creating Pie Chart
+		var myPieChart = new Chart($ctxP).Doughnut(pieData);
+	}
+})
